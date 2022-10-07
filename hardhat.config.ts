16a45a57bf747e70d84e239a690454f4a3709162
lowkey_anp <alaukikn.pant@gmail.com>
2022-10-07 17:50:56 +0800

    Edit readme and replace env variables.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,21 +7,31 @@ import 'solidity-coverage'
 import '@nomiclabs/hardhat-ethers'
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv'
+import { ethers } from 'hardhat'
 
 dotenv.config()
 
-
-
 const balance = '100000000000000000000000000'
-const accounts =
-  [
-        `0x${process.env.ACCOUNT_ONE}`,
-        `0x${process.env.ACCOUNT_TWO}`,
-        `0x${process.env.ACCOUNT_THREE}`,
-        `0x${process.env.ACCOUNT_FOUR}`,
-        `0x${process.env.ACCOUNT_FIVE}`,
-        `0x${process.env.ACCOUNT_SIX}`,
-      ]
+
+// uncomment if you have set secrets
+// const accounts =
+//   [
+//         `0x${process.env.ACCOUNT_ONE}`,
+//         `0x${process.env.ACCOUNT_TWO}`,
+//         `0x${process.env.ACCOUNT_THREE}`,
+//         `0x${process.env.ACCOUNT_FOUR}`,
+//         `0x${process.env.ACCOUNT_FIVE}`,
+//         `0x${process.env.ACCOUNT_SIX}`,
+//       ]
+
+const accounts = [
+  `0xfee7e465e0ad62432ed944ed350c1e22a3eef570f35b58f87c9be4e24ef2e15a`,
+  `0xe27cc62cc1942ea6045f3b6108b5bd82b060848fdb5ee53ca9ba2b4211af1b94`,
+  `0xdeacd7baa598477585e8093a15f06b2bdb47048a887a4cd421fb1d6eb3a40a6d`,
+  `0xde74a0ba709c8e3429d1b80edc82ddcd9117692ad596ec3091185bf20184ef67`,
+  `0x6fe0320beb191a98c08eeee3e2f271d8276f3f060f8141cd1ceb88a38e86b361`,
+  `0xe9572c11a026ffbfbaa1d3cb74fe599d5b10e486c1c86a11d2650aee1f75a518`,
+]
 
 const config: HardhatUserConfig = {
   solidity: {
